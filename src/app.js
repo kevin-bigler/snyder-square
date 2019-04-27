589995fc87162f5b6275d24da3bd67eca9614220
Kevin Bigler <kevinbigler@gmail.com>
2019-04-27 20:25:08 +0100

    Add all 9 numeral squares with number text in them. Rough draft atm, need to be styled much better

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import initRenderer from './main/initRenderer';
 import Colors from './main/Colors';
 import type {Rect, Size} from './main/types';
 import {getOrigin} from './main/types';
+import * as R from 'ramda';
 
 const colors = new Colors();
 
@@ -44,30 +45,59 @@ const squareBg = {
     }
 };
 
-const numeralSq = {
-    size: {width: square.size.width / 3, height: square.size.height / 3},
-    position: {x: 1 * (square.size.width / 3), y: 2 * (square.size.height / 3)},
-    color: colors.lightGreen,
-    border: {
-        width: 1,
-        color: colors.black
-    }
-};
-
 const bgGraphics = getRectGraphics(squareBg);
 const bgTexture = renderer.generateTexture(bgGraphics);
 const bgSprite = getRectSprite({texture: bgTexture, ...squareBg});
 
-const numeralGraphics = getRectGraphics(numeralSq);
-const numeralTexture = renderer.generateTexture(numeralGraphics);
-const numeralSprite = getRectSprite({texture: numeralTexture, ...numeralSq});
+const createNumeralSq = (number: number | string, position: Position, containerSize: Size) => {
+    const numeralSq = {
+        size: {width: containerSize.width / 3, height: containerSize.height / 3},
+        position: getOrigin(),
+        color: colors.lightGreen,
+        border: {
+            width: 1,
+            color: colors.black
+        }
+    };
+
+    const numeralContainer = new PIXI.Container();
+    numeralContainer.x = position.x * (containerSize.width / 3);
+    numeralContainer.y = position.y * (containerSize.height / 3);
+
+    const numeralGraphics = getRectGraphics(numeralSq);
+    const numeralTexture = renderer.generateTexture(numeralGraphics);
+    const numeralSprite = getRectSprite({texture: numeralTexture, ...numeralSq});
+
+    numeralContainer.addChild(numeralSprite);
+    return numeralContainer;
+};
 
 const squareContainer = new PIXI.Container();
 squareContainer.x = square.position.x;
 squareContainer.y = square.position.y;
 
 squareContainer.addChild(bgSprite);
-squareContainer.addChild(numeralSprite);
+
+const createSq = (number: number, position: Position) => {
+    const num = createNumeralSq(number, position, square.size);
+    const text = new PIXI.Text(number + '', {fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+    num.addChild(text);
+    return num;
+};
+
+// const sprites = [0, 1, 2].map(x =>
+//     [0, 1, 2].map(y =>
+//         createSq({x, y})
+// )).flat();
+
+const getCoords = (num, width) => ({x: num % width, y: Math.floor(num / width)});
+
+const sprites = R.range(0, 9).map(num => {
+    return createSq(num, getCoords(num, 3));
+});
+
+sprites.forEach(numeral =>
+    squareContainer.addChild(numeral));
 
 stage.addChild(squareContainer);
 
@@ -80,16 +110,3 @@ renderer.render(stage);
 //
 //
 // const createSq = getLittleSquare({size: bgSquareSize / 3, borderWidth: 2});
-// const sprites = [0, 1, 2].map(x =>
-//     [0, 1, 2].map(y =>
-//         createSq({x, y})
-// )).flat();
-//
-// console.log('sprites', sprites);
-//
-// sprites
-//     .forEach(sprite => snyderSquareContainer.addChild(sprite));
-//
-//
-// stage.addChild(snyderSquareContainer);
-// renderer.render(stage);
